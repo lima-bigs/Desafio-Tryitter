dc26cbc468d90291d2a33b920cce3d3592b88878
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-05-29 21:55:57 -0300

    Feat: Colocando o input e o botão na tela de login

--- Front-end/tryitter/src/Pages/Login.jsx
+++ Front-end/tryitter/src/Pages/Login.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '../Components/Input';
+import Button from '../Components/Button';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submeter');
+  };
+
   return (
     <div>
       <div>
         logo
       </div>
       <div>
-        Login
+        <h1>Realizar login</h1>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="E-mail"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Button>Entrar</Button>
+        </form>
       </div>
     </div>
   );
