730c77ef576f0e448faa5dc77a3dd454a4a19914
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-05-29 21:00:39 -0300

    Feat: Tela de Login - inicio

---
+++ Front-end/tryitter/src/Pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <div>
+        logo
+      </div>
+      <div>
+        Login
+      </div>
+    </div>
+  );
+}
+
+export default Login;
