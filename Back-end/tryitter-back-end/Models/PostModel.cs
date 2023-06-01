e24c803fe4de35223eff5fe461bdcb534344150d
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2023-06-01 15:30:39 -0300

    Feat: Adicionados os pacotes Entity Framework e SqlServer

---
+++ Back-end/tryitter-back-end/Models/PostModel.cs
@@ -0,0 +1,12 @@
+namespace tryitter_back_end.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string? Content { get; set; } = default!;
+        public string? Image { get; set; } = default!;
+        public int UserId { get; set; } = default!;
+        public virtual User User { get; set; } = default!;
+    }
+}
