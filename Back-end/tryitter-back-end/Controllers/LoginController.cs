9e36cdf8cae0d1336f3e3064aaccdb47c3ee44da
Carlos Alberto Souza Lima Júnior <99992740+lima-bigs@users.noreply.github.com>
2023-06-07 08:17:18 -0300

    Revert "[Backend] Criação de Testes"

--- Back-end/tryitter-back-end/Controllers/LoginController.cs
+++ Back-end/tryitter-back-end/Controllers/LoginController.cs
@@ -4,7 +4,7 @@ using tryitter_back_end.Models;
 using tryitter_back_end.Repositories;
 using tryitter_back_end.Services;
 
-namespace tryitter_back_end.test;
+namespace tryitter_back_end.Controllers;
 
 [ApiController]
 [Route("login")]
@@ -17,9 +17,6 @@ public class LoginController : ControllerBase
         _repository = repository;
     }
 
-    /// <summary>
-    /// Faz login passando email e senha, retornando o usuário e o token para acessar as demais rotas.
-    /// </summary>
     [HttpPost]
     public async Task<ActionResult<LoginResponse>> Login(User user)
     {
